711e0a294a79cc13815b7fb06e264dabef8fc97e
mateusPPinh <rawrenete@gmail.com>
2024-03-14 15:18:53 -0300

    apply improvements to the template

--- src/components/Templates/by-definitions/5050/Template50.stories.tsx
+++ src/components/Templates/by-definitions/5050/Template50.stories.tsx
@@ -5,7 +5,7 @@ import Box from '../../../Boxes/Boxes'
 
 const meta: Meta<typeof Template5050> = {
   title: 'Componentes/Template5050',
-  component: Template5050,
+  component: Template5050
 }
 
 export default meta
@@ -13,10 +13,8 @@ type Story = StoryObj<typeof Template5050>
 
 export const Primary: Story = {
   args: {
-    blockPosition: 1,
+    blockPosition: '1',
     layout: '',
-    maxImagesLeft: 2,
-    maxImagesRight: 2,
     slot_left_items: {
       content: [
         {
@@ -26,8 +24,8 @@ export const Primary: Story = {
             desktop_image_path:
               'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
             image_mobile_path:
-              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
-          },
+              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg'
+          }
         },
         {
           title: 'Título Esquerdo 1',
@@ -36,13 +34,11 @@ export const Primary: Story = {
             desktop_image_path:
               'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
             image_mobile_path:
-              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
-          },
-        },
-      ],
+              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg'
+          }
+        }
+      ]
     },
-    slot_left_bgColor: '#dddcdf',
-    slot_right_bgColor: '#f7e3f7',
     slot_right_items: {
       content: [
         {
@@ -52,8 +48,8 @@ export const Primary: Story = {
             desktop_image_path:
               'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
             image_mobile_path:
-              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
-          },
+              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg'
+          }
         },
         {
           title: 'Título Esquerdo 1',
@@ -62,15 +58,33 @@ export const Primary: Story = {
             desktop_image_path:
               'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
             image_mobile_path:
-              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg',
-          },
-        },
-      ],
+              'http://localhost:3001/uploads/a96788fcc1e5ea73af18-desk-nextproject-panvel_vita_c.jpg'
+          }
+        }
+      ]
     },
     template: 'Template5050',
+    textProps: {
+      color: '#7159c1',
+      as: 'h2',
+      font_size: 23
+    },
+    template_50_options: {
+      align_text_slot_left: 'center',
+      align_text_slot_right: 'center',
+      slot_left_bgColor: 'white',
+      slot_right_bgColor: 'white',
+      anchorHandlerProps: {
+        href: '/',
+        isSlotImageLeftAnchor: false,
+        isSlotLeftTitleAnchor: true,
+        isSlotImageRightAnchor: true,
+        isSlotRightTitleAnchor: true
+      }
+    },
+    target: '_blank'
   },
   render: (args) => {
-    console.log(args, 'argumentos')
     return (
       <Box
         css={{
@@ -79,11 +93,11 @@ export const Primary: Story = {
           display: 'flex',
           margin: 'auto',
           flexDirection: 'column',
-          width: 'calc(100% - 32px)',
+          width: 'calc(100% - 32px)'
         }}
       >
         <Template5050 {...args} />
       </Box>
     )
-  },
+  }
 }
--- src/components/Templates/by-definitions/5050/Template50.tsx
+++ src/components/Templates/by-definitions/5050/Template50.tsx
@@ -1,19 +1,25 @@
-/* eslint-disable @typescript-eslint/strict-boolean-expressions */
-/* eslint-disable @typescript-eslint/naming-convention */
 import React from 'react'
 import Box from '../../../Boxes/Boxes'
-import Text from '../../../Typography'
 import { type Template50Types } from './types'
 import Image from '../../../Image/Image'
+import SlotAnchorHandlers from '../../../utils/SlotAnchorHandlers'
+import TextHandlers from '../../../../components/utils/TextHandlers'
 
 const Template5050: React.FC<Template50Types> = ({
-  slot_left_bgColor,
-  slot_right_bgColor,
   slot_left_items,
-  slot_right_items
+  slot_right_items,
+  blockPosition,
+  template_50_options,
+  textProps,
+  target
 }) => {
   const slot_left = slot_left_items?.content ?? []
   const slot_right = slot_right_items?.content ?? []
+
+  if ((slot_left_items == null) && (slot_right_items == null)) {
+    console.error('No data avaliable on slots')
+    return
+  }
 
   return (
     <Box
@@ -34,14 +40,22 @@ const Template5050: React.FC<Template50Types> = ({
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
-          bgColor: slot_left_bgColor
+          bgColor: template_50_options?.slot_left_bgColor
         }}
       >
         {slot_left.map((item, index) => (
-          <Box
-            key={index}
+         <SlotAnchorHandlers
+           key={index}
+           index={index}
+           isSlotLeftTitleAnchor={template_50_options?.anchorHandlerProps?.isSlotLeftTitleAnchor}
+           isSlotImageLeftAnchor={template_50_options?.anchorHandlerProps?.isSlotImageLeftAnchor}
+           position="slot_left"
+           href={template_50_options?.anchorHandlerProps?.href}
+           target={target}
+         >
+           <Box
             css={{
-              alignItems: 'center',
+              alignItems: template_50_options?.align_text_slot_left,
               justifyContent: 'center',
               display: 'flex',
               margin: 'auto',
@@ -49,7 +63,7 @@ const Template5050: React.FC<Template50Types> = ({
               width: 'calc(100% - 32px)'
             }}
           >
-            {item.image && (
+            {(Boolean(item.image)) && (
               <Image
                 key={`left-image-${index}`}
                 desktop_image_path={item.image.desktop_image_path}
@@ -62,9 +76,28 @@ const Template5050: React.FC<Template50Types> = ({
                 }}
               />
             )}
-            <Text as="strong">{item.title}</Text>
-            <Text as="p">{item.subtitle}</Text>
+            <TextHandlers
+              as={textProps?.as}
+              color={textProps?.color}
+              font_family={textProps?.font_family}
+              font_size={textProps?.font_size}
+              line_height={textProps?.line_height}
+              transform={textProps?.transform}
+            >
+              {item.title}
+            </TextHandlers>
+            <TextHandlers
+              as={textProps?.as}
+              color={textProps?.color}
+              font_family={textProps?.font_family}
+              font_size={textProps?.font_size}
+              line_height={textProps?.line_height}
+              transform={textProps?.transform}
+            >
+              {item.subtitle}
+            </TextHandlers>
           </Box>
+         </SlotAnchorHandlers>
         ))}
       </Box>
       {/* Lado direito */}
@@ -75,14 +108,22 @@ const Template5050: React.FC<Template50Types> = ({
           flexDirection: 'column',
           alignItems: 'center',
           justifyContent: 'center',
-          bgColor: slot_right_bgColor
+          bgColor: template_50_options?.slot_right_bgColor
         }}
       >
         {slot_right.map((item, index) => (
-          <Box
-            key={index}
+         <SlotAnchorHandlers
+          key={index}
+          index={index}
+          isSlotRightTitleAnchor={template_50_options?.anchorHandlerProps?.isSlotRightTitleAnchor}
+          isSlotImageRightAnchor={template_50_options?.anchorHandlerProps?.isSlotImageRightAnchor}
+          position="slot_right"
+          href={template_50_options?.anchorHandlerProps?.href}
+          target={target}
+         >
+           <Box
             css={{
-              alignItems: 'center',
+              alignItems: template_50_options?.align_text_slot_right,
               justifyContent: 'center',
               display: 'flex',
               margin: 'auto',
@@ -90,7 +131,7 @@ const Template5050: React.FC<Template50Types> = ({
               width: 'calc(100% - 32px)'
             }}
           >
-            {item.image && (
+            {(Boolean(item.image)) && (
               <Image
                 key={`right-image-${index}`}
                 desktop_image_path={item.image.desktop_image_path}
@@ -103,9 +144,28 @@ const Template5050: React.FC<Template50Types> = ({
                 }}
               />
             )}
-            <Text as="strong">{item.title}</Text>
-            <Text as="p">{item.subtitle}</Text>
+            <TextHandlers
+              as={textProps?.as}
+              color={textProps?.color}
+              font_family={textProps?.font_family}
+              font_size={textProps?.font_size}
+              line_height={textProps?.line_height}
+              transform={textProps?.transform}
+            >
+              {item.title}
+            </TextHandlers>
+            <TextHandlers
+              as={textProps?.as}
+              color={textProps?.color}
+              font_family={textProps?.font_family}
+              font_size={textProps?.font_size}
+              line_height={textProps?.line_height}
+              transform={textProps?.transform}
+            >
+              {item.subtitle}
+            </TextHandlers>
           </Box>
+         </SlotAnchorHandlers>
         ))}
       </Box>
     </Box>
@@ -113,8 +173,21 @@ const Template5050: React.FC<Template50Types> = ({
 }
 
 Template5050.defaultProps = {
-  slot_left_bgColor: '#7159c1',
-  slot_right_bgColor: '#a69dc6'
+  template_50_options: {
+    slot_left_bgColor: 'white',
+    slot_right_bgColor: 'white',
+    align_text_slot_left: 'center',
+    align_text_slot_right: 'center',
+    anchorHandlerProps: {
+      isSlotLeftTitleAnchor: true,
+      isSlotImageLeftAnchor: true,
+      href: 'http://www.google.com',
+      isSlotRightTitleAnchor: true,
+      isSlotImageRightAnchor: true
+    }
+  },
+  target: '_blank'
+
 }
 
 export default Template5050
