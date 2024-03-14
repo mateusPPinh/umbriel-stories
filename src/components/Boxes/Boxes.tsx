bf1c64bf16b09cc5a5c063b3abf3df4ace28cb14
mateusPPinh <rawrenete@gmail.com>
2024-03-14 15:16:33 -0300

    apply improvements into boxes

--- src/components/Boxes/Boxes.tsx
+++ src/components/Boxes/Boxes.tsx
@@ -1,15 +1,21 @@
 import { type BoxProps } from './types'
 import { processInlineStyles } from '../styles/cssUtils'
+import React from 'react'
+import { useTheme } from 'styled-components'
 
-function Box<C extends React.ElementType = 'div'>({
+function Box<C extends React.ElementType = 'div'> ({
   as,
   children,
   css = {},
+  noWrap = false,
   ...props
 }: BoxProps<C>): React.ReactElement {
+  if (noWrap) {
+    return <React.Fragment>{children}</React.Fragment>
+  }
+  const theme = useTheme()
   const Component = as ?? 'div'
-
-  const inlineStyles = processInlineStyles(css)
+  const inlineStyles = processInlineStyles(css, theme)
 
   return (
     <Component style={inlineStyles} {...props}>
