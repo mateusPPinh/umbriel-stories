d1adbb45992cb793abdc5c731fe73aa23392b8ab
mateusPPinh <rawrenete@gmail.com>
2024-10-18 16:29:59 -0300

    sidemenu improvements

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable multiline-ternary */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { useState } from 'react'
 import { type ReactElement } from 'react'
 import { Container } from './styles'
 import { type SideMenuProps } from './types'
@@ -5,20 +8,91 @@ import { type SideMenuProps } from './types'
 export default function SideMenu({
   menu,
   className,
+  customListStyle,
+  customTitleStyle = '',
+  hiddeSideMenu,
 }: SideMenuProps): ReactElement {
+  const [expandedItems, setExpandedItems] = useState<string[]>([])
+
+  const handleToggleItem = (childrenUniqueId: string, hasChildren: boolean) => {
+    if (!hasChildren) return
+    const isItemExpanded = expandedItems.includes(childrenUniqueId)
+
+    setExpandedItems((prev) =>
+      isItemExpanded
+        ? prev.filter((itemId) => itemId !== childrenUniqueId)
+        : [...prev, childrenUniqueId]
+    )
+  }
+
+  const handleClick = (
+    item: {
+      label: string
+      onClick?: () => void
+      children?: Array<{ label: string; onClick?: () => void }>
+      childrenUniqueId: string
+    },
+    hasChildren: boolean
+  ): void => {
+    if (!hasChildren && item.onClick) {
+      item.onClick()
+    } else {
+      handleToggleItem(item.childrenUniqueId, hasChildren)
+    }
+  }
+
   return (
-    <Container className={className}>
+    <Container className={className} $hiddeSideMenu={hiddeSideMenu}>
       {menu.map((section, index) => (
-        <ul key={index}>
+        <ul key={index} className={customListStyle}>
           <li className="font-heading">{section.title}</li>
           {section.items.map((item, itemIndex) => (
-            <span
-              className="font-heading"
-              key={itemIndex}
-              onClick={item.onClick}
-            >
-              {item.label}
-            </span>
+            <div key={itemIndex}>
+              {item.children &&
+              expandedItems.includes(item.childrenUniqueId) ? (
+                <div className="isChildren">
+                  <span
+                    className={`${customTitleStyle} ${
+                      expandedItems.includes(item.childrenUniqueId)
+                        ? 'highlight'
+                        : ''
+                    }`}
+                    onClick={() => {
+                      handleClick(item, !!item.children)
+                    }}
+                  >
+                    {item.label}
+                  </span>
+                </div>
+              ) : (
+                <span
+                  className={customTitleStyle}
+                  onClick={() => {
+                    handleClick(item, !!item.children)
+                  }}
+                >
+                  {item.label}
+                </span>
+              )}
+
+              {item.children && (
+                <ul
+                  className={`submenu ${
+                    expandedItems.includes(item.childrenUniqueId) ? 'open' : ''
+                  }`}
+                >
+                  {item.children.map((child, childIndex) => (
+                    <li
+                      key={childIndex}
+                      className="submenu-item"
+                      onClick={child.onClick}
+                    >
+                      {child.label}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
           ))}
         </ul>
       ))}
--- src/components/SideMenu/styles.ts
+++ src/components/SideMenu/styles.ts
@@ -1,12 +1,108 @@
-import { styled } from 'styled-components'
+import { css, styled } from 'styled-components'
 
-const Container = styled.div`
+const Container = styled.div<{
+  $isSubmenuChildrenOpen?: boolean
+  $hiddeSideMenu?: boolean
+}>`
   width: 100%;
   max-width: 300px;
   background: ${(props) => props.theme.colors.gray50};
-  height: 100vh;
+  height: calc(100vh - 0px);
   padding: 1.5rem;
   border-right: 1px solid #e4e4e7;
+  display: ${(props) => (props.$hiddeSideMenu ? 'none' : '')};
+  overflow-y: auto;
+
+  ${({ $hiddeSideMenu }) => {
+    if ($hiddeSideMenu === true) {
+      return css`
+        transform: translateY(-20px);
+        transition: opacity 0.5s ease, transform 0.5s ease;
+        pointer-events: none;
+      `
+    }
+  }}
+
+  ul.submenu {
+    padding-left: 20px;
+    display: ${(props) => (props.$isSubmenuChildrenOpen ? '' : 'none')};
+
+    ${({ $isSubmenuChildrenOpen }) => {
+      if ($isSubmenuChildrenOpen) {
+        return css`
+          max-height: 0px;
+          padding-left: 20px;
+          opacity: 0;
+          overflow: hidden;
+          transition: max-height 0.3s ease, opacity 0.3s ease;
+        `
+      }
+    }}
+
+    li.submenu-item {
+      margin: 0.4rem 0;
+      padding: 0;
+      margin-left: 10px;
+      font-family: 'Rubik Variable';
+      font-size: 1rem;
+      list-style: square;
+      visibility: visible;
+      transition: opacity underline 0.2s, margin 0.2s ease;
+      cursor: pointer;
+      letter-spacing: 0 !important;
+      color: #27272a;
+
+      &:hover {
+        opacity: 0.7;
+        text-decoration: underline;
+      }
+    }
+  }
+
+  ul.submenu {
+    padding-left: 20px;
+    display: none;
+  }
+
+  ul.submenu.open {
+    opacity: 1;
+    display: block;
+    transition: max-height 0.3s ease, opacity 0.3s ease;
+    height: auto;
+  }
+
+  .highlight {
+    font-family: 'Rubik Variable';
+    line-height: 110%;
+    color: ${(props) => props.theme.colors.gray800};
+    font-size: 16px;
+    font-weight: 700;
+    color: ${(props) => props.theme.colors.lightBlue.lightBlue800};
+  }
+
+  .isChildren {
+    width: 100%;
+    background-color: #e0f2fe;
+    padding: 0.5rem;
+    border-radius: 2px;
+  }
+  ul.submenu.open .submenu-item {
+    margin: 1rem 0;
+    padding: 0;
+    margin-left: 5px;
+    font-family: 'Rubik Variable';
+    font-size: 1rem;
+    list-style: square;
+    visibility: visible;
+    transition: opacity underline 0.2s, margin 0.2s ease;
+    cursor: pointer;
+    color: #27272a;
+
+    &:hover {
+      opacity: 0.7;
+      text-decoration: underline;
+    }
+  }
 
   ul {
     display: flex;
