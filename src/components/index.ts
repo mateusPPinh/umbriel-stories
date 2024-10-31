0d9c8508d451cc9b9ce43845a0dd21d69fb8f7d8
mateusPPinh <rawrenete@gmail.com>
2024-10-31 13:00:27 -0300

    missing imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -33,6 +33,13 @@ export { default as TemplateSeventyThirtyWithCarousel } from './NewPageBlock/tem
 export { default as LazyImage } from './LazyImage'
 export { default as T7030WithinNewsletter } from './NewPageBlock/templates/variations/T7030WithinNewsletter'
 export { default as Article } from './Article'
+export { default as Author } from './Article/Author'
+export { default as Body } from './Article/Body'
+export { default as Share } from './Article/Share'
+export { default as Subtitle } from './Article/Subtitle'
+export { default as Title } from './Article/Title'
 
 // theme
 export * from '../styles/deliveryThemeFunc'
+// types
+export * from './Article/types'
