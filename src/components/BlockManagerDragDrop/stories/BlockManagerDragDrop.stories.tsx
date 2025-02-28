eab85fe4c2fd7c3e867a2d2201b1ddac1fdbfb29
mateusPPinh <rawrenete@gmail.com>
2025-02-28 17:22:44 -0300

    organize storybook

--- src/components/BlockManagerDragDrop/stories/BlockManagerDragDrop.stories.tsx
+++ src/components/BlockManagerDragDrop/stories/BlockManagerDragDrop.stories.tsx
@@ -11,7 +11,7 @@ interface BlockManagerDragDropProps {
 }
 
 export default {
-  title: 'Components/BlockManagerDragDrop',
+  title: 'Components/BlockManager/DragDrop',
   component: BlockManagerDragDrop,
   parameters: {
     layout: 'fullscreen',
@@ -39,101 +39,116 @@ const Template: Story<BlockManagerDragDropProps> = (args) => (
 );
 
 // Grid Variants
-export const StandardGrid = Template.bind({});
-StandardGrid.args = {
+export const Grid_Standard = Template.bind({});
+Grid_Standard.args = {
   blockType: 'grid',
   articles: mockArticles,
   isDarkTheme: false,
   onSave: (columns) => console.log('Updated columns:', columns),
 };
+Grid_Standard.storyName = 'Grid/Standard';
 
-export const FeaturedGrid = Template.bind({});
-FeaturedGrid.args = {
-  ...StandardGrid.args,
+export const Grid_Featured = Template.bind({});
+Grid_Featured.args = {
+  ...Grid_Standard.args,
   blockType: 'grid',
 };
+Grid_Featured.storyName = 'Grid/Featured';
 
-export const MasonryGrid = Template.bind({});
-MasonryGrid.args = {
-  ...StandardGrid.args,
+export const Grid_Masonry = Template.bind({});
+Grid_Masonry.args = {
+  ...Grid_Standard.args,
   blockType: 'grid',
 };
+Grid_Masonry.storyName = 'Grid/Masonry';
 
-export const SidebarGrid = Template.bind({});
-SidebarGrid.args = {
-  ...StandardGrid.args,
+export const Grid_Sidebar = Template.bind({});
+Grid_Sidebar.args = {
+  ...Grid_Standard.args,
   blockType: 'grid',
 };
+Grid_Sidebar.storyName = 'Grid/Sidebar';
 
-export const NewsGrid = Template.bind({});
-NewsGrid.args = {
-  ...StandardGrid.args,
+export const Grid_News = Template.bind({});
+Grid_News.args = {
+  ...Grid_Standard.args,
   blockType: 'grid',
 };
+Grid_News.storyName = 'Grid/News';
 
-export const NewsFeed = Template.bind({});
-NewsFeed.args = {
-  ...StandardGrid.args,
+export const Grid_NewsFeed = Template.bind({});
+Grid_NewsFeed.args = {
+  ...Grid_Standard.args,
   blockType: 'grid',
 };
+Grid_NewsFeed.storyName = 'Grid/News Feed';
 
 // List Variants
-export const ChronologicalList = Template.bind({});
-ChronologicalList.args = {
+export const List_Chronological = Template.bind({});
+List_Chronological.args = {
   blockType: 'list',
   articles: mockArticles,
   isDarkTheme: false,
   onSave: (columns) => console.log('Updated columns:', columns),
 };
+List_Chronological.storyName = 'List/Chronological';
 
-export const CompactList = Template.bind({});
-CompactList.args = {
-  ...ChronologicalList.args,
+export const List_Compact = Template.bind({});
+List_Compact.args = {
+  ...List_Chronological.args,
   blockType: 'list',
 };
+List_Compact.storyName = 'List/Compact';
 
-export const CardList = Template.bind({});
-CardList.args = {
-  ...ChronologicalList.args,
+export const List_Card = Template.bind({});
+List_Card.args = {
+  ...List_Chronological.args,
   blockType: 'list',
 };
+List_Card.storyName = 'List/Card';
 
 // Featured Variants
-export const HeroFeatured = Template.bind({});
-HeroFeatured.args = {
+export const Featured_Hero = Template.bind({});
+Featured_Hero.args = {
   blockType: 'featured',
   articles: mockArticles,
   isDarkTheme: false,
   onSave: (columns) => console.log('Updated columns:', columns),
 };
+Featured_Hero.storyName = 'Featured/Hero';
 
-export const SplitFeatured = Template.bind({});
-SplitFeatured.args = {
-  ...HeroFeatured.args,
+export const Featured_Split = Template.bind({});
+Featured_Split.args = {
+  ...Featured_Hero.args,
   blockType: 'featured',
 };
+Featured_Split.storyName = 'Featured/Split';
 
-export const TripleFeatured = Template.bind({});
-TripleFeatured.args = {
-  ...HeroFeatured.args,
+export const Featured_Triple = Template.bind({});
+Featured_Triple.args = {
+  ...Featured_Hero.args,
   blockType: 'featured',
 };
+Featured_Triple.storyName = 'Featured/Triple';
 
 // Dark Theme Examples
-export const DarkThemeGrid = Template.bind({});
-DarkThemeGrid.args = {
-  ...StandardGrid.args,
+export const Theme_Dark_Grid = Template.bind({});
+Theme_Dark_Grid.args = {
+  ...Grid_Standard.args,
   isDarkTheme: true,
 };
+Theme_Dark_Grid.storyName = 'Theme/Dark/Grid';
 
-export const DarkThemeList = Template.bind({});
-DarkThemeList.args = {
-  ...ChronologicalList.args,
+export const Theme_Dark_List = Template.bind({});
+Theme_Dark_List.args = {
+  ...List_Chronological.args,
   isDarkTheme: true,
 };
+Theme_Dark_List.storyName = 'Theme/Dark/List';
 
-export const DarkThemeFeatured = Template.bind({});
-DarkThemeFeatured.args = {
-  ...HeroFeatured.args,
+export const Theme_Dark_Featured = Template.bind({});
+Theme_Dark_Featured.args = {
+  ...Featured_Hero.args,
   isDarkTheme: true,
-}; 
+};
+Theme_Dark_Featured.storyName = 'Theme/Dark/Featured'; 
